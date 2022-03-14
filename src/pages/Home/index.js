57c6b571c702f1c8c219fcbebc54a844b09de7c1
Gustavo Barros <gustavoasb@gmail.com>
2022-03-14 18:17:07 -0300

    feat: add pokemon picker to home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import Container from '../../components/PageContainer';
 import TradeBox from '../../components/TradeBox';
+import PokemonPicker from '../../components/PokemonPicker';
 
 function Home() {
   return (
     <Container>
       <TradeBox />
+      <PokemonPicker />
     </Container>
   );
 }
