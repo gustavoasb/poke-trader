dcc6fde7a982ca98bd662046cbcf517eddd1c95e
Gustavo Barros <gustavoasb@gmail.com>
2022-03-14 18:16:19 -0300

    feat: pokemon picker base styles

---
+++ src/components/PokemonPicker/index.jsx
@@ -0,0 +1,16 @@
+import { PickerContainer, SearchBarContainer, PokemonList } from "./style";
+
+function PokemonPicker(){
+  return(
+    <PickerContainer>
+      <SearchBarContainer>
+        
+      </SearchBarContainer>
+      <PokemonList>
+
+      </PokemonList>
+    </PickerContainer>
+  )
+}
+
+export default PokemonPicker;
