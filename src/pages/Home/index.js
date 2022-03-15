347cde287d1f2e4a6d2399efac78ce298d8272f2
Gustavo Barros <gustavoasb@gmail.com>
2022-03-15 16:37:58 -0300

    feat: add to trade logic

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,35 @@
 import Container from '../../components/PageContainer';
 import TradeBox from '../../components/TradeBox';
 import PokemonPicker from '../../components/PokemonPicker';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Home() {
+
+  const [pokemons, setPokemons] = useState([])
+  const [pokemonsRed, setPokemonsRed] = useState([])
+  const [pokemonsBlue, setPokemonsBlue] = useState([])
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/")
+      .then((res) => {
+        setPokemons(res.data.results)
+      })
+  }, [])
+
+  const getPokemon = (pokemon, isBlue) => {
+    console.log(pokemon, isBlue)
+    if(isBlue){
+      setPokemonsBlue([...pokemonsBlue, pokemon])
+    } else{
+      setPokemonsRed([...pokemonsRed, pokemon]);
+    } 
+  }
+
   return (
     <Container>
-      <TradeBox />
-      <PokemonPicker />
+      <TradeBox pokemonsBlue={pokemonsBlue} pokemonsRed={pokemonsRed} />
+      <PokemonPicker pokemons={pokemons} getPokemon={getPokemon}/>
     </Container>
   );
 }
