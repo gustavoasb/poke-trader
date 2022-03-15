ccc0318bf532a091428042ee6ec285142ae97006
Gustavo Barros <gustavoasb@gmail.com>
2022-03-15 16:40:21 -0300

    style: trade team box flex

--- src/components/TradeBox/TradeTeamBox/style.js
+++ src/components/TradeBox/TradeTeamBox/style.js
@@ -10,13 +10,6 @@ export const TradeTeamBoxContainer = styled.div`
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.05);
   border-style: solid;
   border-color: #D9DAE8;
-  /* border-style: solid;
-  border-color: transparent;
-  border-color: #D9DAE8; */
-  /* background:linear-gradient(to right,#D9DAE8, transparent, #D9DAE8, transparent) border-box;
-  -webkit-mask:
-    linear-gradient(#fff 0 0) padding-box, 
-    linear-gradient(#fff 0 0);
-  -webkit-mask-composite: xor;
-          mask-composite: exclude; */
+  display: flex;
+  flex-wrap: wrap;
 `
