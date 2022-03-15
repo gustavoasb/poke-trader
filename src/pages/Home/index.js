8fe2823a67c2f637abcbded5f2fe9f4e1ff4c71c
Gustavo Barros <gustavoasb@gmail.com>
2022-03-15 17:52:51 -0300

    feat: add trade info

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,19 @@
 import Container from '../../components/PageContainer';
 import TradeBox from '../../components/TradeBox';
+import TradeInfo from '../../components/TradeInfo';
 import PokemonPicker from '../../components/PokemonPicker';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 function Home() {
 
-  const [pokemons, setPokemons] = useState([])
-  const [pokemonsRed, setPokemonsRed] = useState([])
-  const [pokemonsBlue, setPokemonsBlue] = useState([])
+  const [pokemons, setPokemons] = useState([]);
+  const [pokemonsRed, setPokemonsRed] = useState([]);
+  const [pokemonsBlue, setPokemonsBlue] = useState([]);
+  const [madeTrade, setMadeTrade] = useState(0);
+  let fairness = useRef(0);
+  let redExp = useRef(0);
+  let blueExp = useRef(0);
 
   useEffect(() => {
     axios.get("https://pokeapi.co/api/v2/pokemon/")
@@ -18,7 +23,6 @@ function Home() {
   }, [])
 
   const getPokemon = (pokemon, isBlue) => {
-    console.log(pokemon, isBlue)
     if(isBlue){
       setPokemonsBlue([...pokemonsBlue, pokemon])
     } else{
@@ -26,10 +30,25 @@ function Home() {
     } 
   }
 
+  function pokemonListExperience(array){
+    let experience = array.reduce((acc, pokemon ) => {
+      return acc += pokemon.base_experience;
+    }, 0);
+    return experience;
+  }
+
+  const checkTrade = async () => {
+    redExp.current = await pokemonListExperience(pokemonsRed);
+    blueExp.current = await pokemonListExperience(pokemonsBlue)
+    fairness.current = await Math.abs(blueExp.current - redExp.current);
+    setMadeTrade(key => key + 1);
+  }
+  
   return (
     <Container>
-      <TradeBox pokemonsBlue={pokemonsBlue} pokemonsRed={pokemonsRed} />
+      <TradeBox pokemonsBlue={pokemonsBlue} pokemonsRed={pokemonsRed} checkTrade={checkTrade}/>
       <PokemonPicker pokemons={pokemons} getPokemon={getPokemon}/>
+      {madeTrade > 0 && <TradeInfo redExp={redExp.current} blueExp={blueExp.current} fairness={fairness.current}/>}
     </Container>
   );
 }
