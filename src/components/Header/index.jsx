93d7c7b90028feaf55592be0be99cacc60d623f0
Gustavo Barros <gustavoasb@gmail.com>
2022-03-16 22:48:47 -0300

    style: trade history button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,13 @@
 import { HeaderContainer, HeaderTitle, HistoryButton } from "./style";
+import { FiLayers } from 'react-icons/fi';
 
 function Header({openModal}){
   return(
     <HeaderContainer>
       <HeaderTitle>Pokémon Trade Checker</HeaderTitle>
       <HistoryButton onClick={openModal}>
-        My Trades
+        <FiLayers size={"1rem"}/>
+        &nbsp;My Trades
       </HistoryButton>
     </HeaderContainer>
   )
